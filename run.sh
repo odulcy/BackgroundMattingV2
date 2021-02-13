d89479d492f486ffca77648e471d654cdf33dcbc
Olivier Dulcy <olivier.dulcy@gmail.com>
2021-02-13 20:54:32 +0100

    Add script to run the model

---
+++ run.sh
@@ -0,0 +1,1 @@
+python inference_webcam.py --model-backbone-scale 0.3 --model-type mattingrefine --model-backbone resnet50 --model-checkpoint pytorch_resnet50.pth --camera-device 1 --resolution 800 450
